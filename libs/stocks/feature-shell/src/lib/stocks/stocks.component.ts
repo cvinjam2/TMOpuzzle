9c9e02bd23dd8cb7fdaf1b8bf891ce12e87f6422
cvinjam <cvinjam@admins-MBP.home>
2020-03-17 03:54:44 -0700

    Removed favourite dropdown and implemented differenceInCalendar years.

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import { Observable } from 'rxjs';
+import { differenceInCalendarYears } from 'date-fns';
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -13,24 +15,14 @@ export class StocksComponent implements OnInit {
   period: string;
   fromDate: Date;
   toDate: Date;
-  quotes$ = this.priceQuery.priceQueries$;
+  
+  quotes$: Observable<(string | number)[][]> = this.priceQuery.priceQueries$;
 
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
   maxDate = new Date(); //  date-pickers should not allow selection of dates after the current day.
 
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
     this.stockPickerForm = fb.group({
       symbol: [null, Validators.required],
-      period: [null, Validators.required],
       fromDate: [null, Validators.required],
       toDate: [null, Validators.required]
     });
@@ -40,10 +32,29 @@ export class StocksComponent implements OnInit {
 
   fetchQuote() {
     if (this.stockPickerForm.valid) {
-      const { symbol, period, fromDate, toDate } = this.stockPickerForm.value;
+      const { symbol, fromDate, toDate } = this.stockPickerForm.value;
+      // calculating years instead of passing 'max' to improve performance.
+      const period = this.calucaltePeriod(toDate, fromDate);
       const fromDateNumeric = fromDate.getTime();
       const toDateNumeric = toDate.getTime();
       this.priceQuery.fetchQuote(symbol, period, fromDateNumeric, toDateNumeric);
+    }
+  }
+
+  calucaltePeriod(toDate, fromDate) {
+    const years = differenceInCalendarYears(toDate, fromDate);
+    switch (true) {
+      case (years < 2):
+        return '1y';
+
+      case (years < 3):
+        return '2y';
+
+      case (years < 6):
+        return '5y';
+
+      default:
+        return 'max';
     }
   }
 }
